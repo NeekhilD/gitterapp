65fb2f4ab622c873522b5a195d69ddace3abc5f9
Tomas Vik <tvik@gitlab.com>
2020-01-08 10:50:43 +0100

    pretteir the app.js

--- app.js
+++ app.js
@@ -1,29 +1,29 @@
 /*jshint globalstrict:true, trailing:false, unused:true, node:true */
-"use strict";
+'use strict';
 
-var express         = require('express');
-var passport        = require('passport');
-var OAuth2Strategy  = require('passport-oauth2');
-var request         = require('request');
+var express = require('express');
+var passport = require('passport');
+var OAuth2Strategy = require('passport-oauth2');
+var request = require('request');
 
-var gitterHost    = process.env.HOST || 'https://gitter.im';
-var port          = process.env.PORT || 7000;
+var gitterHost = process.env.HOST || 'https://gitter.im';
+var port = process.env.PORT || 7000;
 
 // Client OAuth configuration
-var clientId      = process.env.GITTER_KEY ? process.env.GITTER_KEY.trim() : undefined;
-var clientSecret  = process.env.GITTER_SECRET ? process.env.GITTER_SECRET.trim() : undefined;
+var clientId = process.env.GITTER_KEY ? process.env.GITTER_KEY.trim() : undefined;
+var clientSecret = process.env.GITTER_SECRET ? process.env.GITTER_SECRET.trim() : undefined;
 
 // Gitter API client helper
 var gitter = {
   fetch: function(path, token, cb) {
     var options = {
-     url: gitterHost + path,
-     headers: {
-       'Authorization': 'Bearer ' + token
-     }
+      url: gitterHost + path,
+      headers: {
+        Authorization: 'Bearer ' + token,
+      },
     };
 
-    request(options, function (err, res, body) {
+    request(options, function(err, res, body) {
       if (err) return cb(err);
 
       if (res.statusCode === 200) {
@@ -44,7 +44,7 @@ var gitter = {
     this.fetch('/api/v1/user/' + user.id + '/rooms', token, function(err, rooms) {
       cb(err, rooms);
     });
-  }
+  },
 };
 
 var app = express();
@@ -54,53 +54,55 @@ app.set('view engine', 'jade');
 app.set('views', __dirname + '/views');
 app.use(express.json());
 app.use(express.urlencoded());
-app.use(express.static( __dirname + '/public'));
+app.use(express.static(__dirname + '/public'));
 app.use(express.bodyParser());
 app.use(express.methodOverride());
 app.use(express.cookieParser());
-app.use(express.session({secret: 'keyboard cat'}));
+app.use(express.session({ secret: 'keyboard cat' }));
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(app.router);
 
 // Passport Configuration
 
-passport.use(new OAuth2Strategy({
-    authorizationURL:   gitterHost + '/login/oauth/authorize',
-    tokenURL:           gitterHost + '/login/oauth/token',
-    clientID:           clientId,
-    clientSecret:       clientSecret,
-    callbackURL:        '/login/callback',
-    passReqToCallback:  true
-  },
-  function(req, accessToken, refreshToken, profile, done) {
-    req.session.token = accessToken;
-    gitter.fetchCurrentUser(accessToken, function(err, user) {
-      return (err ? done(err) : done(null, user));
-    });
-  }
-));
+passport.use(
+  new OAuth2Strategy(
+    {
+      authorizationURL: gitterHost + '/login/oauth/authorize',
+      tokenURL: gitterHost + '/login/oauth/token',
+      clientID: clientId,
+      clientSecret: clientSecret,
+      callbackURL: '/login/callback',
+      passReqToCallback: true,
+    },
+    function(req, accessToken, refreshToken, profile, done) {
+      req.session.token = accessToken;
+      gitter.fetchCurrentUser(accessToken, function(err, user) {
+        return err ? done(err) : done(null, user);
+      });
+    },
+  ),
+);
 
 passport.serializeUser(function(user, done) {
   done(null, JSON.stringify(user));
 });
 
-passport.deserializeUser(function (user, done) {
+passport.deserializeUser(function(user, done) {
   done(null, JSON.parse(user));
 });
 
-app.get('/login',
-  passport.authenticate('oauth2')
-);
+app.get('/login', passport.authenticate('oauth2'));
 
-app.get('/login/callback',
+app.get(
+  '/login/callback',
   passport.authenticate('oauth2', {
     successRedirect: '/home',
-    failureRedirect: '/'
-  })
+    failureRedirect: '/',
+  }),
 );
 
-app.get('/logout', function(req,res) {
+app.get('/logout', function(req, res) {
   req.session.destroy();
   res.redirect('/');
 });
@@ -108,7 +110,6 @@ app.get('/logout', function(req,res) {
 app.get('/', function(req, res) {
   res.render('landing');
 });
-
 
 app.get('/home', function(req, res) {
   if (!req.user) return res.redirect('/');
@@ -121,10 +122,9 @@ app.get('/home', function(req, res) {
       user: req.user,
       token: req.session.token,
       clientId: clientId,
-      rooms: rooms
+      rooms: rooms,
     });
   });
-
 });
 
 app.listen(port);
